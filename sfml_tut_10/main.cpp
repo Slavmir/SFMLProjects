3fe2231d5f51370e08e213b9d8f56ebfc8598dd1
Slawomir Janiga <SJaniga@outlook.com>
2025-12-12 15:06:11 +0100

    Added enemies.

--- sfml_tut_10/main.cpp
+++ sfml_tut_10/main.cpp
@@ -12,34 +12,39 @@
 
 class Bullet
 {
-    public:
-        sf::CircleShape shape;
-        sf::Vector2f currentVelocity;
-        float maxSpeed;
+public:
+    sf::CircleShape shape;
+    sf::Vector2f currentVelocity;
+    float maxSpeed;
 
-        Bullet(float radius = 5.f)
-            : currentVelocity(0.f, 0.f), maxSpeed(15.f){
-            this->shape.setRadius(radius);
-            this->shape.setFillColor(sf::Color::Red);
-        }
+    Bullet(float radius = 5.f)
+        : currentVelocity(0.f, 0.f), maxSpeed(15.f) {
+        this->shape.setRadius(radius);
+        this->shape.setFillColor(sf::Color::Red);
+    }
 };
 
 int main()
 {
+    srand(time(NULL));
     sf::RenderWindow window(sf::VideoMode(800, 600), "360 Shooter!");
     window.setFramerateLimit(60);
-    
+
     //Player
     sf::CircleShape player(25.f);
     player.setFillColor(sf::Color::White);
-    
+
     //Bullet
     Bullet b1;
     std::vector<Bullet> bullets;
     //bullets.push_back(Bullet(b1));
 
     //Enemy
-
+    sf::RectangleShape enemy;
+    std::vector<sf::RectangleShape> enemies;
+    int spawnCounter = 20;
+    enemy.setFillColor(sf::Color::Magenta);
+    enemy.setSize(sf::Vector2f(22.f, 22.f));
 
     //Vectors
     sf::Vector2f playerCenter, mousePosWindow, aimDir, aimDirNormalised;
@@ -57,7 +62,7 @@ int main()
         }
 
         //Update
-        
+
         //Vectors
         playerCenter = sf::Vector2f(player.getPosition().x + player.getRadius(), player.getPosition().y + player.getRadius());
         mousePosWindow = sf::Vector2f(sf::Mouse::getPosition(window));
@@ -73,7 +78,17 @@ int main()
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
             player.move(0.f, 10.f);
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-            player.move(0.f,-10.f);
+            player.move(0.f, -10.f);
+
+        //Enemies
+        if (spawnCounter < 20)
+            spawnCounter++;
+
+        if (spawnCounter >= 20 && enemies.size() < 50) {
+            enemy.setPosition(rand() % window.getSize().x, rand() % window.getSize().y);
+            enemies.push_back(sf::RectangleShape(enemy));
+            spawnCounter = 0;
+        }
 
         //Shooting
         if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
@@ -84,18 +99,29 @@ int main()
 
         for (size_t i = 0; i < bullets.size(); i++) {
             bullets[i].shape.move(bullets[i].currentVelocity);
+            if (bullets[i].shape.getPosition().x < 0 || bullets[i].shape.getPosition().x > window.getSize().x
+                || bullets[i].shape.getPosition().y < 0 || bullets[i].shape.getPosition().y > window.getSize().y){
+                bullets.erase(bullets.begin() + i);
+                }
+            std::cout << bullets.size() << "\n";
         }
+
         
         std::cout << aimDirNormalised.x << " " << aimDirNormalised.y << std::endl;
 
         //Draw
         window.clear();
 
+        for (size_t i = 0; i < enemies.size(); i++) {
+            window.draw(enemies[i]);
+        }
+
         window.draw(player);
 
         for (size_t i = 0; i < bullets.size(); i++) {
             window.draw(bullets[i].shape);
         }
+        window.draw(enemy);
 
         window.display();
     }
