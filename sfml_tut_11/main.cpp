fd4b65d661ccee2388aed762e4517bd4b3f06ffe
Slawomir Janiga <SJaniga@outlook.com>
2025-12-16 10:04:50 +0100

    Text font installed and configured.

--- sfml_tut_11/main.cpp
+++ sfml_tut_11/main.cpp
@@ -4,9 +4,25 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 
+//font Cornelia-BF64e2efc8d82d7
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(640, 400), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Text intro!", sf::Style::Default);
+    window.setFramerateLimit(60);
+    sf::Font font;
+    
+    if (!font.loadFromFile("Fonts/Cornelia-BF64e2efc8d82d7.otf"))
+        throw ("COULD NOT OPEN FONT!");
+
+    //set -> draw below
+    sf::Text text;
+    text.setFont(font);
+    text.setCharacterSize(20);
+    text.setFillColor(sf::Color::Red);
+    text.setStyle(sf::Text::Bold);
+    text.setString("HEY THERE SFML OPERATOR!");
+
 
     while (window.isOpen())
     {
@@ -15,9 +31,16 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+                window.close();
         }
 
+
+        //DRAW
         window.clear();
+
+        //Draw text
+        window.draw(text);
 
         window.display();
     }
